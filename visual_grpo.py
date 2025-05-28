0b30cfa24a8e129864a234f1c9e12bc32cc7aa02
leoairuf <leonardo.furia@outlook.it>
2025-05-28 18:39:16 +0200

    changed params

--- visual_grpo.py
+++ visual_grpo.py
@@ -35,12 +35,12 @@ import types, unsloth_zoo.peft_utils as _pz
 # 0.  GLOBAL HYPERPARAMETERS & CONFIGURATIONS  ─────────────────────────
 # ---------------------------------------------------------------------
 DEFAULT_MODEL_NAME = "unsloth/gemma-3-4b-it"
-DEFAULT_N_SAMPLES_TRAIN = 1000  # Number of samples for quick testing
+DEFAULT_N_SAMPLES_TRAIN = 2  # Number of samples for quick testing
 DEFAULT_MAX_STEPS = 7
 DEFAULT_LEARNING_RATE = 5e-6
 DEFAULT_NUM_GENERATIONS = 6
-DEFAULT_PER_DEVICE_TRAIN_BATCH_SIZE = 1
-DEFAULT_GRADIENT_ACCUMULATION_STEPS = 8
+DEFAULT_PER_DEVICE_TRAIN_BATCH_SIZE = 4
+DEFAULT_GRADIENT_ACCUMULATION_STEPS = 2
 DEFAULT_LORA_R = 32
 DEFAULT_LORA_ALPHA = 32
 DEFAULT_OUTPUT_DIR = "outputs"
@@ -244,7 +244,7 @@ def main():
     print(f"Loading model: {args.model_name}")
     model, tokenizer = FastVisionModel.from_pretrained(
         args.model_name,
-        load_in_4bit=True,
+        load_in_4bit=False,
         load_in_8bit=False,
         use_gradient_checkpointing="unsloth",
     )
@@ -320,7 +320,7 @@ def main():
         optim="paged_adamw_8bit",
         sync_ref_model=True,
         logging_steps=1,
-        max_completion_length=16000,
+        max_completion_length=8000,
         temperature=1.1,
         #evaluation_strategy="steps",
         #eval_steps=args.save_steps,
@@ -330,6 +330,7 @@ def main():
         output_dir=args.output_dir,
         logging_dir=tensorboard_path,
         save_total_limit=1,  # Keep only the latest checkpoint to save disk space
+        log_completions=True,
     )
 
     trainer = GRPOTrainer(
